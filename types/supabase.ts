7facc3fc9188c0dfb9609b96a9face82e1630a40
reallyxilos <sonicforrstest@gmail.com>
2026-05-21 15:22:08 -0400

    "Update_database_types"

--- types/supabase.ts
+++ types/supabase.ts
@@ -3,6 +3,61 @@ export type Json = string | number | boolean | null | { [key: string]: Json } | 
 export interface Database {
   public: {
     Tables: {
+      follows: {
+        Row: {
+          follower_id: string
+          following_id: string
+          created_at: string
+        }
+        Insert: {
+          follower_id: string
+          following_id: string
+          created_at?: string
+        }
+        Update: {
+          follower_id?: string
+          following_id?: string
+          created_at?: string
+        }
+      }
+      posts: {
+        Row: {
+          id: string
+          user_id: string
+          username: string
+          full_name: string
+          is_verified: boolean
+          caption: string | null
+          image_url: string | null
+          likes: number
+          comments: number
+          created_at: string
+        }
+        Insert: {
+          id?: string
+          user_id: string
+          username: string
+          full_name: string
+          is_verified?: boolean
+          caption?: string | null
+          image_url?: string | null
+          likes?: number
+          comments?: number
+          created_at?: string
+        }
+        Update: {
+          id?: string
+          user_id?: string
+          username?: string
+          full_name?: string
+          is_verified?: boolean
+          caption?: string | null
+          image_url?: string | null
+          likes?: number
+          comments?: number
+          created_at?: string
+        }
+      }
       users: {
         Row: {
           id: string
